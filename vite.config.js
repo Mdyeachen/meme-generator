7e3afbcb42e476df2ec649cde123bd0839d7cb73
Yeachen29 <yeachenabir29@gmail.com>
2025-05-09 17:01:34 +0600

    Meme Generator application

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/meme-generator",
   server: {
     port : 8080,
     open : true
